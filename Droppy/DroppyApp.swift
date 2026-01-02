c1bdf381eba84d0036020cad4c326c41a4a06d0e
iordv <jordy.spruit@newyorkpizza.nl>
2026-01-02 10:09:40 +0100

    Initial Commit

---
+++ Droppy/DroppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DroppyApp.swift
+//  Droppy
+//
+//  Created by Jordy Spruit on 02/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DroppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
